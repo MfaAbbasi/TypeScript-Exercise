77a49011aa96905b693d74904edfa97a4fbe06b9
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:22:21 +0500

    Add files via upload

---
+++ 12. Greeting/greeting.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Arsalan', 'Noman', 'Dominic', 'Suneel'];
+const message = 'Eid Mubarak: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12. Greeting/greeting.ts
@@ -0,0 +1,10 @@
+
+let names : string[] = ['Arsalan','Noman','Dominic','Suneel']
+
+const message : string = 'Eid Mubarak: '
+
+for (let i=0; i<names.length; i++){
+
+    console.log(message + names[i])
+
+}
