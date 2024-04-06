f75ba232f27826393f9f0bd5e6179396dbba77a0
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:15:03 +0500

    Add files via upload

---
+++ 07. Number Eight/app.ts
@@ -0,0 +1,16 @@
+
+// Addition
+const resultAdd = 4 + 4;
+console.log(`4 + 4 = ${resultAdd}`);
+
+// Subtraction
+const resultSubtract = 16 - 8;
+console.log(`16 - 8 = ${resultSubtract}`);
+
+// Multiplication
+const resultMultiply = 2 * 4;
+console.log(`2 * 4 = ${resultMultiply}`);
+
+// Division
+const resultDivid = 16 / 2;
+console.log(`16 / 2 = ${resultDivid}`);
