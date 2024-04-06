5c3e103896d9440a5fa4a20ba47f45fb58973483
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:18:19 +0500

    Add files via upload

---
+++ 09. Favorite Number/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let favorite_number = 8;
+let message = favorite_number + ' is my favorite number';
+console.log(message);
---
+++ 09. Favorite Number/app.ts
@@ -0,0 +1,6 @@
+
+let favorite_number : number = 8
+
+let message : string = favorite_number + ' is my favorite number'
+
+console.log(message)
