6c68a07d30e83af1c2c2133e946eaab88bf235ca
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:13:35 +0500

    Add files via upload

---
+++ 06. Stripping Name/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const personName = '    \n\tOkasha\t\n    ';
+console.log(personName);
+const without_Whitespace = personName.trim();
+console.log(without_Whitespace);
---
+++ 06. Stripping Name/app.ts
@@ -0,0 +1,9 @@
+import { log } from "console"
+
+const personName : string = '    \n\tOkasha\t\n    '
+
+console.log (personName)
+
+const without_Whitespace : string = personName.trim()
+
+console.log (without_Whitespace)
