df278fef69cf8eff40babce1476385aaddf4975a
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:23:31 +0500

    Add files via upload

---
+++ 13. Your Own Array/your_own_array.ts
@@ -0,0 +1,9 @@
+
+let transportation : string[] = [ 'Bike' , 'Car' , 'Train' , 'Jeep' ]
+
+for (let i=0; i<transportation.length; i++ ){
+
+    console.log('I would like to own a ' + transportation[i] )
+
+}
+
