05210527f2f3a435ff0d050c3599e143ba82061c
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:16:38 +0500

    Add files via upload

---
+++ 08. Create 4 Lines/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Add
+console.log(4 + 4);
+// Subtract
+console.log(16 - 8);
+// Multiply
+console.log(2 * 4);
+// Divid
+console.log(16 / 2);
---
+++ 08. Create 4 Lines/app.ts
@@ -0,0 +1,12 @@
+
+// Add
+console.log(4 + 4);
+
+// Subtract
+console.log(16 - 8);
+
+// Multiply
+console.log(2 * 4);
+
+// Divid
+console.log(16 / 2);
