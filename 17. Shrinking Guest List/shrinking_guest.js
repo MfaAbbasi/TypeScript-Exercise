9c0113ae021d8b3d990f263a1aba8c82b5133c7a
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:28:33 +0500

    Add files via upload

---
+++ 17. Shrinking Guest List/shrinking_guest.js
@@ -0,0 +1,19 @@
+"use strict";
+let guestList = ['Arsalan', 'Noman', 'Dominic', 'Suneel'];
+let message = 'You are cordially invited to Aftar party: ';
+let absentGuest = 'Dominic';
+let newGuest = 'Taha';
+guestList[2] = newGuest;
+guestList.unshift('Rizwan');
+guestList.splice(2, 0, 'Mubeen');
+guestList.push('Saad');
+console.log('\nUnfortunately, We just found out that our new dinner table won’t arrive in time for the dinner,\nand we have space for only two guests.\n');
+while (guestList.length > 2) {
+    let removeGuest = guestList.pop();
+    console.log(`Dear Mr. ${removeGuest}, \nWe appologize we can not invite you for Aftar party.\n`);
+}
+for (let i = 0; i < guestList.length; i++) {
+    console.log('\nDear Mr. ' + guestList[i] + ',\nYou are still invited.\nThank you!');
+}
+guestList.length = 0;
+console.log('\nupdated guest list:', guestList);
