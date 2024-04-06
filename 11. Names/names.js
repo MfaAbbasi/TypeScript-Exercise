ad67125d53b3adae0515f6900004bc75faa490eb
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-07 02:20:50 +0500

    Add files via upload

---
+++ 11. Names/names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Arsalan', 'Noman', 'Dominic', 'Suneel'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11. Names/names.ts
@@ -0,0 +1,8 @@
+
+let names : string[] = ['Arsalan','Noman','Dominic','Suneel']
+
+for (let i=0; i<names.length; i++){
+
+    console.log(names[i])
+
+}
