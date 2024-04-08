b68eb02a4ffb6f352525f47077bdab9a7eb42644
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-09 03:26:25 +0500

    25 Upload via GitHub Desktop

---
+++ 25. Alien Colors 1/alien_colors1.js
@@ -0,0 +1,10 @@
+"use strict";
+// Create a veriable called alien_color
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player has just earns 05 points!");
+}
+alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("Player has just earns 01 points!");
+}
