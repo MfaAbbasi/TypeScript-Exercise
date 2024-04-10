e93243c5e4e68d9840e1687d203d55def917e619
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-11 03:36:50 +0500

    26 Upload via GitHub Desktop

---
+++ 26. Alien Colors 2/alien_colors2.ts
@@ -0,0 +1,18 @@
+// Create a veriable
+let alien_color : string = 'green'
+
+// If else chain (Alien Color is Green)
+if(alien_color === 'green'){
+    console.log ("Congratulation! You have just earn 05 points for shooting green Alien!")
+}else{
+    console.log ("Congratulation! You have just earns 10 points!")
+}
+
+// If else chain (Alien Color is not Green)
+alien_color = 'yellow'
+
+if(alien_color === 'green'){
+    console.log ("Congratulation! You have just earn 05 points for shooting green Alien!")
+}else{
+    console.log ("Congratulation! You have just earns 10 points!")
+}
