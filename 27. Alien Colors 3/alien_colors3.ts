6426a1921e3da399bf20df842a091f572263b06d
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-11 03:38:52 +0500

    27 Upload via GitHub Desktop

---
+++ 27. Alien Colors 3/alien_colors3.ts
@@ -0,0 +1,37 @@
+let alienColor: string
+
+// Version 1: Alien is green
+alienColor = 'green';
+if (alienColor === 'green') {
+    console.log("Version 1: Congratulations! You just earned 5 points.");
+} else if (alienColor === 'yellow') {
+    console.log("Version 1: Congratulations! You just earned 10 points.");
+} else if (alienColor === 'red') {
+    console.log("Version 1: Congratulations! You just earned 15 points.");
+} else {
+    console.log("Version 1: Unknown alien color. No points awarded.");
+}
+
+// Version 2: Alien is yellow
+alienColor = 'yellow';
+if (alienColor === 'green') {
+    console.log("Version 2: Congratulations! You just earned 5 points.");
+} else if (alienColor === 'yellow') {
+    console.log("Version 2: Congratulations! You just earned 10 points.");
+} else if (alienColor === 'red') {
+    console.log("Version 2: Congratulations! You just earned 15 points.");
+} else {
+    console.log("Version 2: Unknown alien color. No points awarded.");
+}
+
+// Version 3: Alien is red
+alienColor = 'red';
+if (alienColor === 'green') {
+    console.log("Version 3: Congratulations! You just earned 5 points.");
+} else if (alienColor === 'yellow') {
+    console.log("Version 3: Congratulations! You just earned 10 points.");
+} else if (alienColor === 'red') {
+    console.log("Version 3: Congratulations! You just earned 15 points.");
+} else {
+    console.log("Version 3: Unknown alien color. No points awarded.");
+}
