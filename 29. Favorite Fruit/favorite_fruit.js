bf98a7eff568bc600dd197f873e7eaa7dd62c853
Muhammad Fawad <165092699+MfaAbbasi@users.noreply.github.com>
2024-04-11 04:23:43 +0500

    29 Upload via GitHub Desktop

---
+++ 29. Favorite Fruit/favorite_fruit.js
@@ -0,0 +1,23 @@
+"use strict";
+let favoriteFruit = ["apple", "banana", "mango"];
+if (favoriteFruit.includes("apple")) {
+    console.log("Apple is your favorite fruit.");
+}
+if (favoriteFruit.includes("banana")) {
+    console.log("Banana is your favorite fruit.");
+}
+if (favoriteFruit.includes("mango")) {
+    console.log("Mango is your favorite fruit.");
+}
+if (favoriteFruit.includes("kiwi")) {
+    console.log("Kiwi is your favorite fruit.");
+}
+else {
+    console.log("Kiwi is not in your favorite fruit list.");
+}
+if (favoriteFruit.includes("orange")) {
+    console.log("Orange is your favorite fruit");
+}
+else {
+    console.log("Orange is not in your favorite fruit list.");
+}
